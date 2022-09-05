8364c82e04986c9b705a4140ac5d7c0efb5211dd
christiaanNagel <61054009+christiaanNagel@users.noreply.github.com>
2022-09-05 05:25:37 +0200

    Update responsivity of cards

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,77 +5,80 @@ import styles from '../styles/Home.module.scss'
 const Navigation = () => {
     return (
         <>
-        {/* //Desktop navigation */}
-        <div className={styles.navbar}>
-            <div className={styles.navbarImage}>
-                <Image src="/home/exigent-logo.png" width={124} height={24} />
+            {/* //Desktop navigation */}
+            <div className={styles.navbar}>
+                <div className={styles.navbarImage}>
+                    <Image src="/home/exigent-logo.png" width={124} height={24} />
+                </div>
+                <ul className={styles.navbarMenu}>
+                    <Link href="./">
+                        <li>
+                            Item 1
+                        </li>
+                    </Link>
+                    <Link href="./item2">
+                        <li>
+                            Item 2
+                        </li>
+                    </Link>
+                    <Link href="./item3">
+                        <li>
+                            Item 3
+                        </li>
+                    </Link>
+                    <Link href="./item4">
+                        <li>
+                            Item 4
+                        </li>
+                    </Link>
+                    <Link href="./item5">
+                        <li>
+                            Item 5
+                        </li>
+                    </Link>
+                </ul>
+                <button href="#" className={styles.navbarButton}>Button</button>
             </div>
-            <ul className={styles.navbarMenu}>
-                <Link href="./">
-                    <li>
-                        Item 1
-                    </li>
-                </Link>
-                <Link href="./item2">
-                <li>
-                    Item 2
-                </li>
-                </Link>
-                <Link href="./item3">
-                <li>
-                    Item 3
-                </li>
-                </Link>
-                <Link href="./item4">
-                <li>
-                    Item 4
-                </li>
-                </Link>
-                <Link href="./item5">
-                <li>
-                    Item 5
-                </li>
-                </Link>
-            </ul>
-            <button href="#" className={styles.navbarButton}>Button</button>
-        </div>
-        {/* Mobile navigation */}
-        <div className={styles.navbarMobile}>
-            <div className={styles.navbarImage}>
-                <Image src="/home/exigent-logo.png" width={124} height={24} />
+            {/* Mobile navigation */}
+            <div className={styles.navbarMobile}>
+            <div className={styles.navbarHamburgerImageMobile}>
+                    <Image src="/home/hamburgerMenu.png" width={32} height={32} />
+                </div>
+                <div className={styles.navbarImageMobile}>
+                    <Image src="/home/exigent-logo.png" width={124} height={24} />
+                </div>
+                <ul className={styles.navbarMenuMobile}>
+                    <Link href="./">
+                        <li>
+                            Item 1
+                        </li>
+                    </Link>
+                    <Link href="./item2">
+                        <li>
+                            Item 2
+                        </li>
+                    </Link>
+                    <Link href="./item3">
+                        <li>
+                            Item 3
+                        </li>
+                    </Link>
+                    <Link href="./item4">
+                        <li>
+                            Item 4
+                        </li>
+                    </Link>
+                    <Link href="./item5">
+                        <li>
+                            Item 5
+                        </li>
+                    </Link>
+                </ul>
+                <button href="#" className={styles.navbarButtonMobile}>Button</button>
             </div>
-            <ul className={styles.navbarMenuMobile}>
-                <Link href="./">
-                    <li>
-                        Item 1
-                    </li>
-                </Link>
-                <Link href="./item2">
-                <li>
-                    Item 2
-                </li>
-                </Link>
-                <Link href="./item3">
-                <li>
-                    Item 3
-                </li>
-                </Link>
-                <Link href="./item4">
-                <li>
-                    Item 4
-                </li>
-                </Link>
-                <Link href="./item5">
-                <li>
-                    Item 5
-                </li>
-                </Link>
-            </ul>
-            <button href="#" className={styles.navbarButtonMobile}>Button</button>
-        </div>
         </>
 
-        
+
     );
 }
 
